7c70c1c4a3f613aa75724aa2dac3a2cbd2074278
arpitagit-hub <lizachakraborty004@gmail.com>
2025-09-04 01:26:12 +0530

    Add files via upload

---
+++ sumoftwonumber.java
@@ -0,0 +1,22 @@
+package day1;
+
+import java.util.Scanner;
+
+public class sumoftwonumber {
+
+	public static void main(String[] args) {
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("wellcome to calculator");
+		System.out.print("enter first number: ");
+		int firstnum = Sc.nextInt();
+		System.out.print("enter second number: ");
+		int secondnum = Sc.nextInt();
+		int sum = firstnum + secondnum;
+		System.out.println("sum of your number is: " +sum);
+		
+		
+		
+
+	}
+
+}
